9601851f240a04655c644f87cdee422f5ab5dcc0
samsad25-ctrl <samsad25@gmail.com>
2026-08-06 22:26:23 +0800

    Initialize script.js with basic setup

---
+++ script.js
@@ -0,0 +1,10 @@
+/* ===========================================
+   GrammatikLupe
+   Version 0.1
+=========================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("GrammatikLupe gestartet.");
+
+});
